ca8346513e4948c3d857efed52280c64d8872f0c
de-freitas <de-freitas@007-Air.bbrouter>
2024-08-04 17:11:52 -0300

    finish frontend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,169 @@
+"use client";
+
+import { FormEvent, useState } from "react";
+import { useRouter } from "next/navigation";
+import { DateRange } from "react-day-picker";
+import InviteGuestModal from "@/components/create-trip/invite-guests-modal";
+import ConfirmTripModal from "@/components/create-trip/confirm-trip-modal";
+import DestinationAndDateStep from "@/components/create-trip/steps/destination-and-date-step";
+import InviteGuestsStep from "@/components/create-trip/steps/invite-guests-step";
+import { api } from "@/lib/axios";
+
+import logoTripPlanner from "../../public/Logo.svg";
 import Image from "next/image";
 
-export default function Home() {
+export default function CreateTripPage() {
+  const router = useRouter();
+
+  const [isGestsInputOpen, setIsGuestsInputOpen] = useState(false);
+  const [isGuestsModalOpen, setIsGuestsModalOpen] = useState(false);
+  const [emailsToInvite, setEmailsToInvite] = useState([
+    "shakira@gmail.com.br",
+    "keanu.reeves@gmail.com",
+  ]);
+  const [isConfirmTripModalOpen, setIsConfirmTripModalOpen] = useState(false);
+
+  const [destination, setDestination] = useState("");
+  const [ownerName, setOwnerName] = useState("");
+  const [ownerEmail, setOwnerEmail] = useState("");
+  const [eventStartAndEndDates, setEventStartAndEndDates] = useState<
+    DateRange | undefined
+  >();
+
+  function openGuestsInput() {
+    setIsGuestsInputOpen(true);
+  }
+
+  function closeGuestsInput() {
+    setIsGuestsInputOpen(false);
+  }
+
+  function openGuestsModal() {
+    setIsGuestsModalOpen(true);
+  }
+
+  function closeGuestsModal() {
+    setIsGuestsModalOpen(false);
+  }
+
+  function openConfirmTripModal() {
+    if (emailsToInvite.length === 0) return;
+    setIsConfirmTripModalOpen(true);
+  }
+
+  function closeConfirmTripModal() {
+    setIsConfirmTripModalOpen(false);
+  }
+
+  function addNewEmailToInvite(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const data = new FormData(event.currentTarget);
+    const email = data.get("email")?.toString();
+
+    if (!email) return;
+    if (emailsToInvite.includes(email))
+      return window.alert("e-mail já cadastrado");
+
+    setEmailsToInvite([...emailsToInvite, email]);
+
+    event.currentTarget.reset();
+  }
+
+  function removeEmailsFromInvite(emailToRemove: string) {
+    const newEmailList = emailsToInvite.filter(
+      (invited) => invited !== emailToRemove
+    );
+
+    setEmailsToInvite(newEmailList);
+  }
+
+  async function createTrip(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    if (!destination) return;
+    if (!eventStartAndEndDates?.from || !eventStartAndEndDates?.to) return;
+    if (emailsToInvite.length === 0) return;
+    if (!ownerEmail || !ownerEmail) return;
+
+    const response = await api.post("/trips", {
+      destination: destination,
+      starts_at: eventStartAndEndDates?.from,
+      ends_at: eventStartAndEndDates?.to,
+      emails_to_invite: emailsToInvite,
+      owner_name: ownerName,
+      owner_email: ownerEmail,
+    });
+
+    const { tripId } = response.data;
+    //router.push(`/trips/${tripId}`);
+    router.push(`/trips/${tripId}`);
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <div className="bg-pattern bg-no-repeat bg-center h-screen flex items-center justify-center">
+        <div className="max-w-3xl w-full px-6 text-center space-y-10">
+          <div className="flex flex-col items-center gap-3">
+            <Image src={logoTripPlanner} alt="Trip Planner logo" />
+            <p className="text-zinc-300 text-lg">
+              Convide seus amigos e planeje sua próxima viagem!
+            </p>
+          </div>
+
+          <div className="space-y-4">
+            {
+              <DestinationAndDateStep
+                closeGuestsInput={closeGuestsInput}
+                openGuestsInput={openGuestsInput}
+                isGestsInputOpen={isGestsInputOpen}
+                setDestination={setDestination}
+                eventStartAndEndDates={eventStartAndEndDates}
+                setEventStartAndEndDates={setEventStartAndEndDates}
+              />
+            }
+
+            {isGestsInputOpen && (
+              <InviteGuestsStep
+                openGuestsModal={openGuestsModal}
+                emailsToInvite={emailsToInvite}
+                openConfirmTripModal={openConfirmTripModal}
+              />
+            )}
+          </div>
+
+          <p className="text-sm text-zinc-500">
+            Ao planejar sua viagem pela plann.er você automaticamente
+            <br />
+            concorda com nossos{" "}
+            <a className="text-zinc-300 underline" href="#">
+              termos de uso
+            </a>{" "}
+            e{" "}
+            <a className="text-zinc-300 underline" href="#">
+              políticas de privacidade
+            </a>
+          </p>
         </div>
+
+        {isGuestsModalOpen && (
+          <InviteGuestModal
+            emailsToInvite={emailsToInvite}
+            addNewEmailToInvite={addNewEmailToInvite}
+            closeGuestsModal={closeGuestsModal}
+            removeEmailsFromInvite={removeEmailsFromInvite}
+          />
+        )}
+
+        {isConfirmTripModalOpen && (
+          <ConfirmTripModal
+            closeConfirmTripModal={closeConfirmTripModal}
+            createTrip={createTrip}
+            setOwnerName={setOwnerName}
+            setOwnerEmail={setOwnerEmail}
+          />
+        )}
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
